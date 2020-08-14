4689458e01b1931b2ca379a8caf81628d02888dc
Soumik <soumik.dutta@sixt.com>
2020-08-14 20:06:59 +0530

    Initial commit

---
+++ src/main/java/com/soumik/postgreselasticintegration/PostgresElasticIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.soumik.postgreselasticintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresElasticIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresElasticIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soumik/postgreselasticintegration/PostgresElasticIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumik.postgreselasticintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresElasticIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
